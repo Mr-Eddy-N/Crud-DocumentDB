4029f2f409066efa980fd7097a55f030d06f9106
Edgar Reyes Rojas <EdgarReyes@itesm.mx>
2017-02-08 17:59:24 -0600

    first

---
+++ DocumentDB/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace DocumentDB
+{
+   public class Program
+    {
+        private const string EndpointUri = "https://localhost:8081/";
+        private const string PrimaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        private DocumentClient client;
+
+        static void Main(string[] args)
+        {
+            DocumentClient client = 
+                new DocumentClient(
+                    new Uri("https://localhost:8081"),
+                    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+
+            try
+            {
+                Program p = new Program();
+                p.GetStartedDemo().Wait();
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        // ADD THIS PART TO YOUR CODE
+        private void WriteToConsoleAndPromptToContinue(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+        // ADD THIS PART TO YOUR CODE
+        private async Task CreateDatabaseIfNotExists(string databaseName)
+        {
+            // Check to verify a database with the id=FamilyDB does not exist
+            try
+            {
+                await this.client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(databaseName));
+                this.WriteToConsoleAndPromptToContinue("Found {0}", databaseName);
+            }
+            catch (DocumentClientException de)
+            {
+                // If the database does not exist, create a new database
+                if (de.StatusCode == HttpStatusCode.NotFound)
+                {
+                    this.client.CreateDatabaseAsync(new Database { Id = databaseName });
+                    this.WriteToConsoleAndPromptToContinue("Created {0}", databaseName);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+
+        private async Task GetStartedDemo()
+        {
+            this.client = new DocumentClient(new Uri(EndpointUri), PrimaryKey);
+
+            // ADD THIS PART TO YOUR CODE
+            await this.CreateDatabaseIfNotExists("FamilyDB_oa");
+        }
+
+
+
+
+    }
+}
